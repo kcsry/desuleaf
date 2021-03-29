ac0267222e2dacadbf1ea75f64fe5ba31cc03b25
Aarni Koskela <akx@iki.fi>
2021-03-29 21:05:20 +0300

    Heh

--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,22 @@ import leaf2 from "./img/leaf2.png";
 import leaf3 from "./img/leaf3.png";
 import leaf4 from "./img/leaf4.png";
 import birb from "./mus/birb.mp3";
+import bad from "./mus/bad.mp3";
+import tada from "./mus/tada.mp3";
+import music from "./mus/win.mp3";
 import pr3 from "./mus/pr3.mp3";
 import desumask from "./desumask";
 import gothicImg from "./img/gothic_0.png";
 import gothicCfg from "./img/gothic.xml?url";
 
-const WIDTH = 600;
+const WIDTH = 580;
 const HEIGHT = 400;
 const MAX_BLOW_DIST = 50;
 const MAX_BLOW_DIST_SQR = MAX_BLOW_DIST * MAX_BLOW_DIST;
 const FORCE_MUL = 50;
-const VEL_MUL = 0.5;
+const VEL_MUL = 0.55;
+const TOTAL_TIME = 100;
+const GOOD_SCORE = 60;
 
 function choice<T>(arr: readonly T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -36,13 +41,23 @@ const LOOP_CONFIG = {
   },
 };
 
+function getRank(score: number): string {
+  const letter = "SABCDFGQXÖ"[Math.floor(9 - score / 10)];
+  const plus = score % 10 > 8 ? "+" : score % 10 < 3 ? "-" : "";
+  return letter + plus;
+}
+
 class GameScene extends Phaser.Scene {
   private desukun: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody = null as any;
   private particles: Phaser.GameObjects.Particles.ParticleEmitterManager = null as any;
   private leaves: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody[] = [];
   private emitter: Phaser.GameObjects.Particles.ParticleEmitter = null as any;
   private scoreText: Phaser.GameObjects.BitmapText = null as any;
-  private motor: Phaser.Sound.BaseSound = null as any;
+  private rankText: Phaser.GameObjects.BitmapText = null as any;
+  private motor: Phaser.Sound.WebAudioSound = null as any;
+  private timer: Phaser.Time.TimerEvent = null as any;
+  private score = 0;
+  private timeElapsed = 0;
   constructor() {
     super({ key: "GameScene" });
   }
@@ -58,6 +73,9 @@ class GameScene extends Phaser.Scene {
     this.load.image("leaf4", leaf4);
     this.load.audio("birb", birb);
     this.load.audio("motor", pr3);
+    this.load.audio("bad", bad);
+    this.load.audio("tada", tada);
+    this.load.audio("music", music);
 
     this.load.bitmapFont("gothic", gothicImg, gothicCfg);
   }
@@ -67,21 +85,20 @@ class GameScene extends Phaser.Scene {
 
     const scale = 260;
     const leafSpriteNames = ["leaf1", "leaf2", "leaf3", "leaf4"];
-    for (let i = 0; i < 700; i++) {
+    for (let i = 0; i < 750; i++) {
       const [fx, fy] = choice(desumask);
       const x = 160 + fx * scale + Math.random() * 3;
       const y = 15 + fy * scale + Math.random() * 3;
       const spr = this.physics.add.sprite(x, y, choice(leafSpriteNames));
       spr.flipX = Math.random() < 0.5;
       spr.flipY = Math.random() < 0.5;
-      spr.body.setDrag(150, 150);
+      spr.body.setDrag(rand(100, 150), rand(100, 150));
       spr.body.setAngularDrag(500);
       // spr.body.setFriction(0.3, 0.3);
       this.leaves.push(spr);
     }
 
     this.desukun = this.physics.add.sprite(55, 55, "desukun1");
-    this.desukun.setCollideWorldBounds(true);
 
     this.particles = this.add.particles("puff");
 
@@ -99,15 +116,61 @@ class GameScene extends Phaser.Scene {
     this.scoreText = this.add.bitmapText(5, 5, "gothic", "hello");
     this.scoreText.blendMode = "ADD";
 
+    this.rankText = this.add.bitmapText(WIDTH / 2, HEIGHT / 2, "gothic", "");
+    this.rankText.setOrigin(0.5, 0.5);
+    this.rankText.setScale(3, 3);
+    this.rankText.blendMode = "ADD";
+
     const birbs = this.sound.add("birb");
     birbs.addMarker(LOOP_CONFIG);
     birbs.play("loop");
-    this.motor = this.sound.add("motor");
+    this.motor = this.sound.add("motor") as Phaser.Sound.WebAudioSound;
     this.motor.addMarker(LOOP_CONFIG);
-    this.motor.play("loop");
+    this.motor.setVolume(0);
+
+    this.timer = this.time.addEvent({
+      delay: 100,
+      loop: true,
+      paused: true,
+      callback: this.timerCallback,
+    });
   }
 
+  private timerCallback = () => {
+    this.timeElapsed++;
+    if (Math.floor(this.timeElapsed) === TOTAL_TIME + 10) {
+      this.sound.add(this.score >= GOOD_SCORE ? "tada" : "bad").play();
+      this.rankText.setText("RANK " + getRank(this.score));
+      if (this.score >= GOOD_SCORE) {
+        this.time.addEvent({
+          delay: 1500,
+          callback: () => {
+            this.sound.add("music").play();
+          },
+        });
+      }
+    }
+  };
+
   update(): void {
+    this.score = this.computeScore();
+    const timeRemaining = Math.max(0, TOTAL_TIME - this.timeElapsed);
+    this.scoreText.setText(`Score: ${this.score}% - Time: ${timeRemaining}`);
+    this.regularGameplay(timeRemaining);
+  }
+
+  private computeScore() {
+    const rawScore =
+      this.leaves.reduce((sc, l) => {
+        if (l.x < 0 || l.x > WIDTH || l.y < 0 || l.y > HEIGHT) {
+          return sc + 1;
+        }
+        return sc;
+      }, 0) / this.leaves.length;
+    return Math.round(rawScore * 100);
+  }
+
+  private regularGameplay(timeRemaining: number): void {
     const desukun = this.desukun;
     // TODO: touch too, not just mouse?
     const ptr = this.input.mousePointer;
@@ -117,7 +180,15 @@ class GameScene extends Phaser.Scene {
         ? ["desukun1", "desukun2"][Math.floor(this.game.getTime() / 100) % 2]
         : "desukun1"
     );
-    if (ptr.isDown && !desukun.body.hitTest(ptr.x, ptr.y)) {
+    if (this.timeElapsed >= TOTAL_TIME + 25 && this.score >= GOOD_SCORE) {
+      // DANCE FOR ME
+      desukun.flipX = Math.floor(this.timeElapsed / 2) % 2 == 0;
+    }
+    if (
+      ptr.isDown &&
+      Phaser.Math.Distance.BetweenPointsSquared(desukun, ptr) > 45 &&
+      timeRemaining > 0
+    ) {
       desukun.flipX = desukun.body.velocity.x > 0;
       this.emitter.followOffset.set(-60 * (desukun.flipX ? -1 : 1), 0);
       this.physics.moveToObject(desukun, ptr, 180);
@@ -150,19 +221,17 @@ class GameScene extends Phaser.Scene {
       const vel = desukun.body.velocity.scale(0.9);
       desukun.setVelocity(vel.x, vel.y);
     }
-    const rawScore =
-      this.leaves.reduce((sc, l) => {
-        if (l.x < 0 || l.x > WIDTH || l.y < 0 || l.y > HEIGHT) {
-          return sc + 1;
-        }
-        return sc;
-      }, 0) / this.leaves.length;
-    const score = Math.round(rawScore * 100);
-    this.scoreText.setText(`Score: ${score}%`);
 
     this.emitter.on = blows > 10;
-    if (blows > 5) this.motor.resume();
-    else this.motor.pause();
+    if (blows > 5) {
+      if (!this.motor.isPlaying) {
+        this.motor.play("loop");
+      }
+      this.motor.setVolume(1);
+      this.timer.paused = false;
+    } else {
+      this.motor.setVolume(this.motor.volume * 0.95);
+    }
   }
 }
 
